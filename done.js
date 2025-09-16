2260189dc42ba9291bc027576dac796b3e85eb9a
Igor-Parov <ihor207415@student-schoolsofwestfield.org>
2025-09-16 12:12:14 -0400

    Update done.js

--- done.js
+++ done.js
@@ -45,3 +45,59 @@ alert ("My favorite color is "+ color);
 
 
 /* Prompt with Conditional */
+/* Var, Prompt and Alert Katas */
+
+/* COLOR KATAS */
+
+/* No Prompt */
+// make a string variable for a color, set it equal to your favorite
+var color = "green";
+// alert the message" "My favorite color is " plus the color
+alert ("My favorite color is "+ color);
+
+/* Prompt */
+
+
+/* AGE */
+
+/* No Prompt */
+
+/* Prompt */
+var age = 17;
+// make a string variable for name, set it to your name
+var fname = "Igor"
+// alert name is age years old
+alert (fname + "is" + age + "years old");
+
+/* Prompt with Conditional */
+
+
+/* ICE CREAM */
+var flavor = prompt("favorite flavor?");
+var scoops = prompt("number of scoops?");
+alert("you want" + scoops + "scoops of" + flavor)
+/* No Prompt */
+
+
+/* Prompt */
+
+
+/* Prompt with Conditional */
+
+
+/* PET */
+
+/* No Prompt */
+var petType = prompt("What is your pet type?");
+var petName = prompt("What is your pet's name?");
+alert ("You have a " + petType + " named " + petName);
+
+/* Prompt */
+
+
+/* Prompt with Conditional */
+var petType = prompt("What is your pet type?");
+var petName = prompt("What is your pet's name?");
+alert ("You have a " + petType + " named " + petName);
+if (petType = "dog") prompt("I like dogs too!");
+if (petType = "cat") prompt("I'm allergic to cats");
