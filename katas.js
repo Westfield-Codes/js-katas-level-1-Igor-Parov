0368cbe5b8d1c49a11c3a2a326f0cb3add19d9f3
Igor-Parov <ihor207415@student-schoolsofwestfield.org>
2025-10-08 20:06:34 -0400

    Update katas.js

--- katas.js
+++ katas.js
@@ -7,7 +7,7 @@
 /* Var Alert */
 // make a string variable named color, set it equal to your favorite color.
 // alert the message" "My favorite color is " plus the variable name, outside the quotes.
-
+1234
 /* Var Alert Prompt */
 // make a string variable for color, but prompt the user for their favorite color.
 // alert color plus " is a nice color!" - note the space in the quotes.
